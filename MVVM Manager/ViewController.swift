d299ae87deb84e36c940ec6edaf5b340616106d4
Burenkovuser <nb2207@yandex.ru>
2019-10-16 18:21:19 +0700

    Initial Commit

---
+++ MVVM Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Manager
+//
+//  Created by Vasilii on 16.10.2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
